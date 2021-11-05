feb799d407d782eafdf3c12dec9d12080d0b587f
BADCoCoBunny <gracebradlol@gmail.com>
2021-11-04 21:32:17 -0500

    Métodos para la busqueda de datos y la eliminación agregados

--- Listas/ListaSimple.py
+++ Listas/ListaSimple.py
@@ -35,7 +35,7 @@ class ListaSimple:
                     nodo_actual = self.nodoHead
                     nodo_preActual = None
                     index = 0
-                    while(nodo_actual.siguiente != None):
+                    while(nodo_actual != None):
                         if (index == posicion):
                             nodo_entrada.siguiente = nodo_actual
                             nodo_preActual.siguiente = nodo_entrada
@@ -71,24 +71,74 @@ class ListaSimple:
             nodo_actual.siguiente = nodo_entrada
 
     # Eliminar
-    def eliminar(self, dato) -> None:
+    def eliminar(self, dato, eliminar_todos=False) -> None:
+        """
+        Elimina la primera o todas las apariciones de un dato en la lista.
+        Parametros: object - dato a eliminar
+        boolean - true para eliminar todas las apariciones
+        """
         if (not self.estaVacia()):
-            nodo_actual = self.nodoHead
-            nodo_preActual = None
-            while(nodo_actual.siguiente != None):
-                if (nodo_actual.dato == dato):
-                    if (nodo_preActual != None):
-                        nodo_preActual.siguiente = nodo_actual.siguiente
-                    else:
+            if(eliminar_todos):
+                while(self.apariciones(dato) > 0):
+                    self.eliminar(dato)
+            else:
+                if (self.buscar(dato) == 0):
+                    self.eliminarInicial
+                if (self.buscar(dato) == self.longitud()-1):
+                    self.eliminarFinal()
+                nodo_actual = self.nodoHead
+                nodo_preActual = None
+                while(nodo_actual != None):
+                    if (nodo_actual.dato == dato):
+                        if (nodo_preActual != None):
+                            nodo_preActual.siguiente = nodo_actual.siguiente
+                        else:
+                            self.eliminarInicial()
+                        break
+                    nodo_preActual = nodo_actual
+                    nodo_actual = nodo_actual.siguiente
+
+    def eliminarPos(self, posicion, eliminar_todos=False) -> None:
+        """
+        Elimina la primera o todas las apariciones de un dato en una posición
+        especifica de la lista.
+        Parametros: int - posición del dato a eliminar
+        boolean - true para eliminar todas las apariciones
+        """
+        if (not self.estaVacia()):
+            if (0 <= posicion < self.longitud()):
+                if (eliminar_todos):
+                    dato = self.buscarPos(posicion)
+                    while (self.apariciones(dato) > 0):
+                        self.eliminar(dato)
+                else:
+                    if (posicion == 0):
                         self.eliminarInicial()
-                nodo_preActual = nodo_actual
-                nodo_actual = nodo_actual.siguiente
+                    elif (posicion == self.longitud()-1):
+                        self.eliminarFinal()
+                    else:
+                        nodo_actual = self.nodoHead
+                        nodo_preActual = None
+                        index = 0
+                        while (nodo_actual.siguiente != None):
+                            if (index == posicion):
+                                nodo_preActual.siguiente = nodo_actual.siguiente
+                                break
+                            nodo_preActual = nodo_actual
+                            nodo_actual = nodo_actual.siguiente
+                            index += 1
 
     def eliminarInicial(self) -> None:
+        """
+        Elimina el primer dato de la lista.
+        """
         if(not self.estaVacia()):
             self.nodoHead = self.nodoHead.siguiente
 
     def eliminarFinal(self) -> None:
+        """
+        Elimina el último dato de la lista.
+        """
         if (not self.estaVacia()):
             nodo_actual = self.nodoHead
             nodo_preActual = None
@@ -96,6 +146,73 @@ class ListaSimple:
                 nodo_preActual = nodo_actual
                 nodo_actual = nodo_actual.siguiente
             nodo_preActual.siguiente = None
+
+    # Buscar
+    def buscar(self, dato) -> int:
+        """
+        Busca la primera posición en la que aparece un dato especifico.
+        Parametros: object - dato a buscar
+        Retorno: int - posicion del dato buscado
+        """
+        if (not self.estaVacia()):
+            nodo_actual = self.nodoHead
+            index = 0
+            while(nodo_actual != None):
+                if (nodo_actual.dato == dato):
+                    return index
+                nodo_actual = nodo_actual.siguiente
+                index += 1
+            return -1  # Si no hay resultados
+        else:
+            return -1
+
+    def buscarPos(self, posicion) -> object:
+        """
+        Busca un dato de la lista indicando la posición especifica.
+        Parametros: int - posición del dato a buscar
+        Retorno: object - dato en la posición especificada
+        """
+        if (not self.estaVacia()):
+            if (0 <= posicion < self.longitud()):
+                if (posicion == 0):
+                    return self.buscarInicial()
+                elif (posicion == self.longitud()-1):
+                    return self.buscarFinal()
+                else:
+                    nodo_actual = self.nodoHead
+                    index = 0
+                    while (nodo_actual != None):
+                        if (index == posicion):
+                            return nodo_actual.dato
+                        nodo_actual = nodo_actual.siguiente
+                        index += 1
+            else:
+                return None  # Posición no valida
+        else:
+            return None  # Lista vacía
+
+    def buscarInicial(self) -> object:
+        """
+        Retorna el dato inicial de la lista.
+        Retorno: object - primer dato
+        """
+        if(not self.estaVacia()):
+            return self.nodoHead.dato
+        else:
+            return None
+
+    def buscarFinal(self) -> object:
+        """
+        Retorna el dato final de la lista.
+        Retorno: object - último dato
+        """
+        if (not self.estaVacia()):
+            nodo_actual = self.nodoHead
+            while (nodo_actual.siguiente != None):
+                nodo_actual = nodo_actual.siguiente
+            return nodo_actual.dato
+        else:
+            return None
 
     # Información
     def apariciones(self, dato) -> int:
@@ -109,7 +226,7 @@ class ListaSimple:
         else:
             nodo_actual = self.nodoHead
             apariciones = 0
-            while(nodo_actual.siguiente != None):
+            while(nodo_actual != None):
                 if (nodo_actual.dato == dato):
                     apariciones += 1
                 nodo_actual = nodo_actual.siguiente
@@ -164,45 +281,57 @@ class ListaSimple:
             return str(self.dato)
 
 
-print("\n>> Creando una lista...")
-lista_numeros = ListaSimple()
-print("¿Está vacía? " + str(lista_numeros.estaVacia()))
-print("Longitud: " + str(lista_numeros.longitud()))
-print("Lista: " + str(lista_numeros))
+if __name__ == "__main__":
+    print("\n>> Creando una lista...")
+    lista_numeros = ListaSimple()
+    print("¿Está vacía? " + str(lista_numeros.estaVacia()))
+    print("Longitud: " + str(lista_numeros.longitud()))
+    print("Lista: " + str(lista_numeros))
 
-print("\n>> Adicionando elementos [1, 2, 3]...")
-lista_numeros.adicionarElemento(1)
-lista_numeros.adicionarElemento(2)
-lista_numeros.adicionarElemento(3)
-print("Lista: " + str(lista_numeros))
-print("Longitud: " + str(lista_numeros.longitud()))
+    print("\n>> Adicionando elementos [1, 2, 3]...")
+    lista_numeros.adicionarElemento(1)
+    lista_numeros.adicionarElemento(2)
+    lista_numeros.adicionarElemento(3)
+    print("Lista: " + str(lista_numeros))
+    print("Longitud: " + str(lista_numeros.longitud()))
 
-print("\n>> Adicionando un elemento al inicio (0)...")
-lista_numeros.adicionarInicio(0)
-print("Lista: " + str(lista_numeros))
-print("Longitud: " + str(lista_numeros.longitud()))
+    print("\n>> Adicionando un elemento al inicio (0)...")
+    lista_numeros.adicionarInicio(0)
+    print("Lista: " + str(lista_numeros))
+    print("Longitud: " + str(lista_numeros.longitud()))
 
-print("\n>> Adicionando un elemento al final (4)...")
-lista_numeros.adicionarFinal(4)
-print("Lista: " + str(lista_numeros))
-print("Longitud: " + str(lista_numeros.longitud()))
+    print("\n>> Adicionando un elemento al final (4)...")
+    lista_numeros.adicionarFinal(4)
+    print("Lista: " + str(lista_numeros))
+    print("Longitud: " + str(lista_numeros.longitud()))
 
-print("\n>> Adicionando un elemento en una posición especifica (2020, 3)...")
-lista_numeros.adicionarElemento(2020, 3)
-print("Lista: " + str(lista_numeros))
-print("Longitud: " + str(lista_numeros.longitud()))
+    print("\n>> Adicionando un elemento en una posición especifica (2020, 3)...")
+    lista_numeros.adicionarElemento(2020, 3)
+    print("Lista: " + str(lista_numeros))
+    print("Longitud: " + str(lista_numeros.longitud()))
 
-print("\n>> Eliminando un elemento especifico (2020)...")
-lista_numeros.eliminar(2020)
-print("Lista: " + str(lista_numeros))
-print("Longitud: " + str(lista_numeros.longitud()))
+    print("\n>> Eliminando un elemento especifico (2020)...")
+    lista_numeros.eliminar(2020, False)
+    print("Lista: " + str(lista_numeros))
+    print("Longitud: " + str(lista_numeros.longitud()))
 
-print("\n>> Eliminando inicial...")
-lista_numeros.eliminarInicial()
-print("Lista: " + str(lista_numeros))
-print("Longitud: " + str(lista_numeros.longitud()))
+    print("\n>> Eliminando un elemento en una posición especifica (3)...")
+    lista_numeros.eliminarPos(3)
+    print("Lista: " + str(lista_numeros))
+    print("Longitud: " + str(lista_numeros.longitud()))
 
-print("\n>> Eliminando final...")
-lista_numeros.eliminarFinal()
-print("Lista: " + str(lista_numeros))
-print("Longitud: " + str(lista_numeros.longitud()))
+    print("\n>> Eliminando inicial...")
+    lista_numeros.eliminarInicial()
+    print("Lista: " + str(lista_numeros))
+    print("Longitud: " + str(lista_numeros.longitud()))
+
+    print("\n>> Eliminando final...")
+    lista_numeros.eliminarFinal()
+    print("Lista: " + str(lista_numeros))
+    print("Longitud: " + str(lista_numeros.longitud()))
+
+    print("\n>> Buscando la posicion de un dato (2)...")
+    print("Posición: " + str(lista_numeros.buscar(2)))
+
+    print("\n>> Buscando el dato contenido en un posición especifica (1)...")
+    print("Dato: " + str(lista_numeros.buscarPos(1)))
